97cdb9aa2b024c13651d852c160ad818b04a73dc
Diana Jordan <dianacjordan@gmail.com>
2023-04-18 17:35:52 -0400

    ud seeder

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -32,42 +32,44 @@ if __name__ == '__main__':
         session.add(manager)
 
 
-
-############ * EMPLOYEE * #############
-
-
-    position = ["Jr. Dev", "Sr. Dev", "UX Specialist"]
-
-    session.query(Employee).delete()
-    for i in range(10):
-        employee = Employee(
-            name = faker.name(),
-            email = faker.email(),
-            phone_number = random.randint(1000000000, 9999999999),
-            position = random.choice(position),
-            manager_id = random.randint(1, 10),
-            project_id = random.randint(1, 10)
-        )
-
-        print(employee)
-        session.add(employee)
-
     
 ############ * PROJECTS * #############
-
-    projects = ["Samsung", "Project_2", "Project_3", "Project_4", "Project_5", "Project_6"]
-    
     session.query(Project).delete()
+    projects = ["Samsung", "Project_2", "Project_3", "Project_4", "Project_5", "Project_6"]
+
     for i in range(10):
+    
         project = Project(
             name = random.choice(projects),
             description = faker.sentence(nb_words=10, variable_nb_words=False),
             manager_id = random.randint(1, 10),
         )
 
+
         print(project)
         session.add(project)
+        session.commit()
+        
 
+    
+############ * EMPLOYEE * #############
+    session.query(Employee).delete()
+
+    position = ["Jr. Dev", "Sr. Dev", "UX Specialist"]
+
+    for i in range(10):
+        rand_proj = random.choice(session.query(Project).all())
+        employee = Employee(
+            name = faker.name(),
+            email = faker.email(),
+            phone_number = random.randint(1000000000, 9999999999),
+            position = random.choice(position),
+            project_id = rand_proj.id,
+            manager_id = rand_proj.manager_id
+        )
+        
+        print(employee)
+        session.add(employee)
 
 
     session.commit()
